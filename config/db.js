d7b74f38303a0e8aa6bdac22a17e20b1f6bf0b1b
Alavazarez2004 <145495596+Alavazarez2004@users.noreply.github.com>
2024-12-11 03:58:10 -0600

    Actualizacion PORT

--- config/db.js
+++ config/db.js
@@ -5,6 +5,7 @@ const sequelize = new Sequelize(
   process.env.DB_NAME, // Nombre de la base de datos
   process.env.DB_USER, // Usuario de la base de datos
   process.env.DB_PASS, // Contraseña de la base de datos
+  process.env.DB_PORT,
   {
     host: process.env.DB_HOST,   // Host de la base de datos
     dialect: 'mysql',            // Especifica el dialecto, puede ser 'mysql', 'postgres', 'sqlite', etc.
